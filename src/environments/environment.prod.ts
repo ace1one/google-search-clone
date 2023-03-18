9fe084a5b1d58b6f633f04d90c19807752716e53
cogent <ashim.kc@cogenthealth.com.np>
2023-03-18 19:13:52 +0545

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true
+  apiUrl:  'https://customsearch.googleapis.com',
+  apiKey: 'AIzaSyA51ipRRxur4iOiVEW6D7oz52Lg32QG-UI',
+  apiCX: '2260b12bab7874968'
 };
